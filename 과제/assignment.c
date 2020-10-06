78a45f1ce7ed35dabad5ecf8dc5f4dfc82d126c7
Owner <forest7592@hanmail.net>
2020-10-06 20:30:53 +0900

    5주차 실습-4

---
+++ 과제/assignment.c
@@ -0,0 +1,9 @@
+//4주차 과제 대용 파일
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!!");
+	printf("Hello World2!!");
+	return 0;
+}
---
+++ 실습/practice.c
@@ -0,0 +1,9 @@
+//4주차 실습 대용 파일
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!!");
+	printf("Hello World2!!");
+	return 0;
+}
